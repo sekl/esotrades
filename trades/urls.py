f15690213dfb481b8b2f9806a81c783ca9248ee7
sekl <sebastian.klier@gmx.de>
2014-02-20 12:08:36 +0100

    Add pagination to trades listing'

--- trades/urls.py
+++ trades/urls.py
@@ -4,7 +4,7 @@ from trades import views
 
 urlpatterns = patterns('',
 
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.IndexView.as_view(paginate_by=10), name='index'),
     url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^new$', login_required(views.CreateView.as_view()), name="new_trade"),
 )
